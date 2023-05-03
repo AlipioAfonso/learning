2beeaea9bb2b9c492e9cba76c2c956268244f53e
Alípio Afonso de Almeida Oliveira <alipio@alipio.eu>
2023-05-03 10:54:13 -0300

    Added get shopping list from firebase and append to list

--- firebase/firebase-app/index.js
+++ firebase/firebase-app/index.js
@@ -1,30 +1,55 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-a8608-default-rtdb.firebaseio.com"
 }
 
+// Methods related to the firebase
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListDB = ref(database, "shoppingList")
+
+// Get and append the shopping list on the ul list
+onValue(shoppingListDB, function(snapshot) {
+
+    let shoppingListValues = Object.values(snapshot.val())
+
+    shoppingListValues.forEach(shoppingItem => {
 
 
+        console.log(shoppingItem)
+        appendToShoppingList(shoppingItem)
+    });
+
+})
+
+// Variables from the DOM
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-const shoppingList = document.getElementById("list")
+const shoppingListEl = document.getElementById("list")
+
+
+// Functions 
 
 function clearInputFieldEl(inputField) {
     // Cleans the add-input field
     inputField.value = ""
 }
 
-function appendChildOnToParent(parent, child = "div", text = "") {
+function appendChildToParent(parent, child = "div", text = "") {
     let node = document.createElement(child)
     let textNode = document.createTextNode(text)
     node.appendChild(textNode)
     parent.appendChild(node)
 }
+
+
+function appendToShoppingList(shoppingItem) {
+    appendChildToParent(shoppingListEl, "li", shoppingItem)
+}
+
+// Events 
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
@@ -34,7 +59,7 @@ addButtonEl.addEventListener("click", function() {
         // Clean the input field
     clearInputFieldEl(inputFieldEl)
         // Appends a node to another node with text
-    appendChildOnToParent(shoppingList, "li", inputValue)
+    appendChildOnToParent(shoppingListEl, "li", inputValue)
     console.log(inputValue)
 
 })
