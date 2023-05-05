29657c31758bdad148bf9da400899a3ce2be6867
Alípio Afonso de Almeida Oliveira <alipio@alipio.eu>
2023-05-04 22:05:34 -0300

    Added icons, webmanifest, modified html and js

--- firebase/firebase-app/index.js
+++ firebase/firebase-app/index.js
@@ -1,26 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-a8608-default-rtdb.firebaseio.com"
 }
 
+const dbName = "shoppingList"
+
 // Methods related to the firebase
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListDB = ref(database, "shoppingList")
+const shoppingListDB = ref(database, dbName)
+
+
 
 // Get and append the shopping list on the ul list
 onValue(shoppingListDB, function(snapshot) {
+    clearInnerElement(shoppingListEl)
+    if (snapshot.exists()) {
+        let shoppingListArrays = Object.entries(snapshot.val())
 
-    let shoppingListValues = Object.values(snapshot.val())
 
-    shoppingListValues.forEach(shoppingItem => {
+        shoppingListArrays.forEach(shoppingItem => {
 
+            appendToShoppingList(shoppingItem)
+        });
 
-        console.log(shoppingItem)
-        appendToShoppingList(shoppingItem)
-    });
+    } else {
+        shoppingListEl.textContent = "Essa lista ainda não tem itens..."
+    }
+
 
 })
 
@@ -37,16 +46,38 @@ function clearInputFieldEl(inputField) {
     inputField.value = ""
 }
 
-function appendChildToParent(parent, child = "div", text = "") {
-    let node = document.createElement(child)
-    let textNode = document.createTextNode(text)
+function appendChildToParent(element) {
+    let node = document.createElement(element.child)
+    let textNode = document.createTextNode(element.text)
     node.appendChild(textNode)
-    parent.appendChild(node)
+    element.parent.appendChild(node)
+    node.addEventListener("dblclick", function() {
+        console.log(element.id)
+        let exactLocationOfShoppingDB = ref(database, `${dbName}/${element.id}`)
+
+        remove(exactLocationOfShoppingDB)
+    })
 }
 
 
 function appendToShoppingList(shoppingItem) {
-    appendChildToParent(shoppingListEl, "li", shoppingItem)
+    let itemID = shoppingItem[0]
+    let itemValue = shoppingItem[1]
+
+    appendChildToParent({
+        id: itemID,
+        parent: shoppingListEl,
+        child: "li",
+        text: itemValue
+    })
+
+
+
+}
+
+// Clear the shopping list 
+function clearInnerElement(element) {
+    element.textContent = ""
 }
 
 // Events 
@@ -54,12 +85,15 @@ function appendToShoppingList(shoppingItem) {
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
+    clearInnerElement(shoppingListEl)
+
     // Put the input value on the database
-    push(shoppingListInDB, inputValue)
+    push(shoppingListDB, inputValue)
         // Clean the input field
     clearInputFieldEl(inputFieldEl)
-        // Appends a node to another node with text
-    appendChildOnToParent(shoppingListEl, "li", inputValue)
+
+    // Appends a node to another node with text
+    //appendChildToParent(shoppingListEl, "li", inputValue)
     console.log(inputValue)
 
 })
